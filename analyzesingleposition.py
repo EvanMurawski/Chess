48bc3662f376f3b017b0d3bf7d05cc3720d2985e
Evan Murawski <evan.murawski@gmail.com>
2022-10-17 23:05:23 -0400

    Added piece factors for knights and bishops

--- analyzesingleposition.py
+++ analyzesingleposition.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     #-------------------
 
 
-    my_fen_board = FENUtil.fenToBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    my_fen_board = FENUtil.fenToBoard("r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3")
     my_fen_board.print()
 
     newBoards = my_fen_board.getLegalMoves()
