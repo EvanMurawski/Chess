e0995cd8f3bbcdc48aab89b8bfdbf5b8a03a3d29
Evan Murawski <evan.murawski@gmail.com>
2022-11-04 22:44:00 -0400

    Added legal move check to test.py and modifed the checkmate testcases.

--- test.py
+++ test.py
@@ -1,40 +1,50 @@
 import unittest
-import pickle
 from FENUtil import FENUtil
 import AlphaBetaNew
 from Node import Node
+from BoardRep import BoardRep
+import chess.pgn
 
 
 from BoardRep import BoardRep
 
-class TestTactics(unittest.TestCase):
+class TestAlphaBeta(unittest.TestCase):
 
+    TACTICS_FILE_NAME = "tactics.txt"
+
+    #TODO: Create more tactics to test
     def testTactics(self):
-        file = open("tactics.txt", 'r')
+        file = open(self.TACTICS_FILE_NAME, 'r')
 
-        i = 0
-        j = 0
+        num_tested = 0
+        num_failed = 0
         while True:
             fen = file.readline()
             if not fen:
                 break
-
             solution = file.readline()
             board = FENUtil.fenToBoard(fen)
             best_node = AlphaBetaNew.getBestMoveMulti(Node(board), board.whitemove)
-            best_move = best_node.move_squares
-            move_string = BoardRep.numbersToAlg(best_move)
+            best_move = [best_node.board, best_node.move_squares]
+            move_string = BoardRep.moveToAlg(best_move)
             try:
                 self.assertEqual(move_string+"\n", solution)
             except AssertionError:
-                j += 1
+                num_failed += 1
                 print("Failed tactic: ", fen)
-            i += 1
+                raise AssertionError
 
-        print("Tested ", i , " tactics, ", j , " failed.")
+            num_tested += 1
+
+        print("Tested ", num_tested , " tactics, ", num_failed , " failed.")
         file.close()
 
-class TestScore(unittest.TestCase):
+class TestBoardRep(unittest.TestCase):
+
+    NUM_TEST_MOVE_GENERATION = 500
+    PGN_FILE_NAME = "20kgames.pgn"
+    CHECKMATES_TEST_FILE = "checkmate_test.txt"
+
 
     def testStartingScore(self):
         board = BoardRep()
@@ -43,57 +53,96 @@ class TestScore(unittest.TestCase):
         self.assertGreater(score, -0.2)
         self.assertLess(score, 0.2)
 
-class TestCheckmate(unittest.TestCase):
 
     def testOneCheckmate(self):
         board = FENUtil.fenToBoard("rnbqkbnr/2pp1Qpp/pp6/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
         self.assertTrue(board.isCheckmate())
 
+
+    def testGetLegalMoves(self):
+        pgn = open(self.PGN_FILE_NAME)
+        num_positions = 0
+
+        for i in range(0, self.NUM_TEST_MOVE_GENERATION):
+
+            game = chess.pgn.read_game(pgn)
+            mainline = game.mainline()
+
+            for move in mainline:
+                fen = move.board().fen()
+                legals = list(move.board().legal_moves)
+                num_legal = len(legals)
+                legals_str = [str(move) for move in legals]
+
+                my_boardrep = FENUtil.fenToBoard(fen)
+                my_moves = my_boardrep.getLegalMoves()
+                my_num_legal = len(my_moves)
+                my_str = [BoardRep.moveToAlg(item) for item in my_moves]
+
+                num_positions += 1
+                try:
+                    self.assertEqual(my_num_legal, num_legal)
+
+                except AssertionError:
+                    print("Error found in game number: ", i)
+                    print(fen)
+
+                    for movestring in my_str:
+                        if movestring not in legals_str:
+                            print("My move ", movestring, " is not in legal moves")
+
+                    for legalmove in legals_str:
+                        if legalmove not in my_str:
+                            print("Legal move ", legalmove, " is not in my moves")
+
+                    raise AssertionError
+
+        pgn.close()
+        print("Tested legal move generation for ", num_positions, " positions. ")
+
+
     def testManyCheckmates(self):
-        f = open('store.pck1', 'rb')
-        fen_checkmates = pickle.load(f)
-        f.close()
+        f = open(self.CHECKMATES_TEST_FILE, 'r')
 
-        i = 0
-        j = 0
-        for fen_checkmate in fen_checkmates:
-            board = FENUtil.fenToBoard(fen_checkmate)
+        num_tests = 0
+        num_failed = 0
+        num_checkmate = 0
+        num_noncheckmate = 0
+        while True:
+            fen = f.readline()
+            if not fen:
+                break
+            board = FENUtil.fenToBoard(fen)
+
+            checkmate_status = f.readline()
             try:
-                self.assertTrue(board.isCheckmate())
-                score = board.getScore()
-                if board.whitemove:
-                    self.assertEqual(score, -999)
+                if checkmate_status == "True\n":
+                    num_checkmate += 1
+                    self.assertTrue(board.isCheckmate())
+                    expected_score = -999 if board.whitemove else 999
+                    self.assertEqual(board.getScore(), expected_score)
                 else:
-                    self.assertEqual(score, 999)
+                    num_noncheckmate += 1
+                    self.assertFalse(board.isCheckmate())
 
             except AssertionError:
-                j += 1
-                print(fen_checkmate)
+                num_failed += 1
+                print("Failed checkmate check in position: \n", fen)
+                f.close()
+                raise AssertionError
 
-            i += 1
+            num_tests += 1
 
-        print("Tested " + str(i) + " checkmates.")
-        print(str(j) + " failed.")
-
-    def testManyNonCheckmates(self):
-        f = open('storenotcheckmate.pck1', 'rb')
-        not_checkmates = pickle.load(f)
         f.close()
+        print("Checked checkmate status for ", num_tests, " positions, ", num_failed, " failed. " ,num_checkmate,
+              " positions were checkmate and ", num_noncheckmate, " positions were not checkmate")
 
-        i = 0
-        j = 0
-        for not_checkmate in not_checkmates:
-            board = FENUtil.fenToBoard(not_checkmate)
-            try:
-                self.assertFalse(board.isCheckmate())
-            except AssertionError:
-                j += 1
-                print(not_checkmate)
 
-            i += 1
+    #TODO: Test case for material
+    #TODO: Test case for illegal move (isincheckotherplayer)
+    #TODO: Test case for stalemate
+    #TODO: Test cases for specific edge cases
 
-        print("Tested " + str(i) + " non-checkmates.")
-        print(str(j) + " failed.")
 
 if __name__ == '__main__':
     unittest.main()
--- testpythonchess.py
+++ testpythonchess.py
@@ -2,69 +2,38 @@ import chess.pgn
 from BoardRep import BoardRep
 from FENUtil import FENUtil
 
-print(chess.__file__)
-pgn = open("20kgames.pgn")
+def generateCheckmateTest():
+    pgn = open("GamesEndingInCheckmate.pgn")
+    write_file= open("checkmate_test.txt", 'w')
+
+    for i in range(0, 5000):
+        if i%10 == 0:
+            print("Checking num: ", i)
+        try:
+            game = chess.pgn.read_game(pgn)
+        except UnicodeDecodeError:
+            print("unicode error")
+        except ValueError:
+            print("value error")
+        except AttributeError:
+            print("att error")
+
+        mainline = game.mainline()
+        for move in mainline:
+            fen = move.board().fen()
+            write_file.write(fen+ "\n")
+
+            if move.board().is_checkmate():
+                write_file.write("True\n")
+            else:
+                write_file.write("False\n")
+
+    pgn.close()
+    write_file.close()
 
 
-for i in range(0, 1000):
-    if i%10 == 0:
-        print("Checking num: ", i)
-    try:
-        game = chess.pgn.read_game(pgn)
-    except UnicodeDecodeError:
-        print("unicode")
-    except ValueError:
-        print("value error")
-    except AttributeError:
-        print("att error")
-
-    mainline = game.mainline()
-    for move in mainline:
-        fen = move.board().fen()
-        legals = list(move.board().legal_moves)
-        num_legal = len(legals)
-        legals_str = [str(move) for move in legals]
-
-        my_boardrep = FENUtil.fenToBoard(fen)
-        my_moves = my_boardrep.getLegalMoves()
-        my_num_legal = len(my_moves)
-        my_str = [BoardRep.moveToAlg(item) for item in my_moves]
-
-        if my_num_legal != num_legal:
-            print("Error found in game number: ", i)
-            print(fen)
-
-            for movestring in my_str:
-                if movestring not in legals_str:
-                    print("My move ", movestring , " is not in legal moves")
-
-            for legalmove in legals_str:
-                if legalmove not in my_str:
-                    print("Legal move ", legalmove, " is not in my moves")
-
-
-
-
-pgn.close()
-
-
-
-#     try:
-#         first_game = chess.pgn.read_game(pgn)
-#         board = first_game.end().board()
-#         if board.fen() != "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" and board.fen() != "rnbqkbnr/pppppppp/8/8/8/7P/PPPPPPP1/RNBQKBNR b KQkq - 0 1" and board.fen() != "rnbqkbnr/pppppppp/8/8/5P2/8/PPPPP1PP/RNBQKBNR b KQkq - 0 1":
-#             fen_checkmates.append(board.fen())
-#     except UnicodeDecodeError:
-#         print("unicode")
-#     except ValueError:
-#         print("value error")
-#
-#
-# print(fen_checkmates[500])
-#
-# f = open('store.pck1', 'wb')
-# pickle.dump(fen_checkmates, f)
-# f.close()
+if __name__ == "__main__":
+    generateCheckmateTest()
 
 
 
